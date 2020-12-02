ccc3519138e89bfadd1b6c0da21c00014dd80fea
Karan <karanwagh17@gmail.com>
2020-12-02 17:37:22 +0530

    added bottom navbar

--- app/src/main/java/com/example/recipeappandroid/MainActivity.java
+++ app/src/main/java/com/example/recipeappandroid/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.recipeappandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.recipeappandroid.Fragments.BookmarkFragment;
+import com.example.recipeappandroid.Fragments.LandingFragment;
+import com.example.recipeappandroid.Fragments.ProfileFragment;
+import com.example.recipeappandroid.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -23,10 +33,12 @@ public class MainActivity extends AppCompatActivity {
                 openSearchActiviy();
             }
         });
+
     }
 
     public  void  openSearchActiviy() {
         Intent intent = new Intent(this, searchActivity.class);
         startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/recipeappandroid/searchActivity.java
+++ app/src/main/java/com/example/recipeappandroid/searchActivity.java
@@ -1,14 +1,55 @@
 package com.example.recipeappandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.recipeappandroid.Fragments.BookmarkFragment;
+import com.example.recipeappandroid.Fragments.LandingFragment;
+import com.example.recipeappandroid.Fragments.ProfileFragment;
+import com.example.recipeappandroid.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class searchActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectedFragment = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new LandingFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_explore:
+                        selectedFragment = new LandingFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_bookmark:
+                        selectedFragment =  new BookmarkFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                if (selectedFragment!=null)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                }
+                return true;
+
+            }
+        });
     }
 }
