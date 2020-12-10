03b7ef991c99a4c3640c49a813e06d835f2ab03d
Sakshi Chaudhari <sakshee1603@gmail.com>
2020-12-10 22:13:05 +0530

    ease in animation- homepage

--- app/src/main/java/com/example/recipeappandroid/MainActivity.java
+++ app/src/main/java/com/example/recipeappandroid/MainActivity.java
@@ -1,24 +1,22 @@
 package com.example.recipeappandroid;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-
-import com.example.recipeappandroid.Fragments.BookmarkFragment;
-import com.example.recipeappandroid.Fragments.LandingFragment;
-import com.example.recipeappandroid.Fragments.ProfileFragment;
-import com.example.recipeappandroid.Fragments.SearchFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private ImageView ImageAnimation;
+    private Animation animTranslate, animScale;
+    private TextView welcome_anim, phrase_anim, hc_anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +28,33 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSearchActiviy();
-            }
+                openSearchActiviy(); }
+
         });
+
+        ImageAnimation = (ImageView)findViewById(R.id.imageView);
+        animTranslate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ttb);
+        ImageAnimation.startAnimation(animTranslate);
+
+        welcome_anim = (TextView)findViewById(R.id.welcome);
+        animTranslate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ttb);
+        welcome_anim.startAnimation(animTranslate);
+
+        phrase_anim = (TextView)findViewById(R.id.phrase);
+        animTranslate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ttb);
+        phrase_anim.startAnimation(animTranslate);
+
+        hc_anim = (TextView)findViewById(R.id.happy_cooking);
+        animTranslate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ttb);
+        hc_anim.startAnimation(animTranslate);
+
+        animScale = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ttb);
+        button.startAnimation(animScale);
 
     }
 
     public  void  openSearchActiviy() {
         Intent intent = new Intent(this, searchActivity.class);
         startActivity(intent);
-
     }
 }
