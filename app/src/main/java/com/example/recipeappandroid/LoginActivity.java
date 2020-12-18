05201b1ba166f535a43dbd9e0a422d32bd8711b8
Karan <karanwagh17@gmail.com>
2020-12-18 23:39:21 +0530

    TextView to RegisterActivity

--- app/src/main/java/com/example/recipeappandroid/LoginActivity.java
+++ app/src/main/java/com/example/recipeappandroid/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.recipeappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView Login = (TextView) findViewById(R.id.account);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+    public void openRegisterActivity(){
+        Intent intent =  new Intent( this, RecipeActivity.class);
+        startActivity(intent);
     }
 }
